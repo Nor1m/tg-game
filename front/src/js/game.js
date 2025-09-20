194ea2a85e0e1746482ec70006c08df992fcb25e
v.mironov <v.mironov@whitewill.ru>
2025-09-20 22:44:53 +0300

    Поправил время для бустов

--- front/src/js/game.js
+++ front/src/js/game.js
@@ -867,7 +867,7 @@
 
             if (Date.now() - lastCoinBoostTime > coinBoostInterval) {
                 spawnPowerUp('coin_boost');
-                coinBoostInterval = Math.random() * 45000;
+                coinBoostInterval = Math.random() * 40000;
             }
 
             increaseDifficulty();
